cfe3ee573b9a787a722bcd06f8b8aa292afa681d
evgen-wtf <guest.job0@gmail.com>
2024-04-27 14:06:49 +0300

    KS-1-2 print variables

---
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -0,0 +1,12 @@
+package lesson_1
+
+fun main() {
+    var numberOfOrders: Int = 75
+    val thanksForPurchase: String = "Спасибо за покупку!"
+    println(numberOfOrders)
+    println(thanksForPurchase)
+    var numberOfWorkers = 2000
+    //println(numberOfWorkers)
+    numberOfWorkers = 1999
+    println(numberOfWorkers)
+}
