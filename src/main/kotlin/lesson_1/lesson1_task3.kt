26a4a9671d15ed2da7404029b039fdb2cf01bbcd
evgen-wtf <guest.job0@gmail.com>
2024-05-01 20:09:04 +0300

    KS-1-3 refactor

--- src/main/kotlin/lesson_1/lesson1_task3.kt
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -2,21 +2,15 @@ package lesson_1
 
 fun main() {
     val year = 1961
-
-//    var hour = 09 // не понимаю,почему int не работает с нулем в начале?
-//    var minute = 07
-
-    var hour: String = 9.toString()
-    var minute: String = 7.toString()
+    var hour: String = "09"
+    var minute: String = "07"
 
     println(year)
     println(hour)
     println(minute)
 
-//    hour = 10 // также не понял, почему нельзя повторно иницилизировать переменную int без приведения к строке?
-//    minute = 55
+    hour = "10"
+    minute = "55"
 
-    hour = 10.toString()
-    minute = 55.toString()
     print("$hour:$minute")
 }
